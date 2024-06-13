91f02064877728c7f7b9cd20b3fe166b8ced13b3
PrinceCarter <princethepharaoh@gmail.com>
2024-06-12 22:14:03 -0400

    add url error handling

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,21 @@ import { fetchMetadata } from "frames.js/next";
 const BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
 
+console.log("BASE_URL:", BASE_URL);
+
 export async function generateMetadata() {
+  let url;
+  try {
+    url = new URL("/frames", BASE_URL);
+  } catch (error) {
+    console.error("Invalid BASE_URL:", BASE_URL);
+    throw error;
+  }
+
   return {
     title: "Kramer Contest",
     other: {
-      ...(await fetchMetadata(new URL("/frames", BASE_URL))),
+      ...(await fetchMetadata(url)),
     },
   };
 }
